b1fbcfe3455de02462b78c7ba6ef37c7e5686623
Greg Flavin <flavingreg90@yahoo.com>
2023-02-17 17:00:24 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Greg Flavin on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
